d2613055e311f4d622cf593556257785c6193f23
himanshu <himanshusarad@gmail.com>
2024-03-18 23:07:04 +0530

    Security filter and Security Context holder

---
+++ src/main/java/com/example/auth/security/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.auth.security.repository;
+
+import com.example.auth.security.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+}
